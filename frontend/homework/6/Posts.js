947eefdbdf73bad3e76cf887f05efe472d124034
arunain23 <arunain.mahant@kickdrumtech.com>
2024-02-13 23:56:34 +0530

    Nodejs advance

---
+++ frontend/homework/6/Posts.js
@@ -0,0 +1,22 @@
+const posts=[
+    {
+        "id":1,
+        "age":23,
+        "name":"amey"
+    },
+    {
+        "id":2,
+        "age":24,
+        "name":"aditya"
+    },
+
+    {
+        "id":3,
+        "age":22,
+        "name":"arun"
+    },
+
+];
+
+module.exports =posts;
+
